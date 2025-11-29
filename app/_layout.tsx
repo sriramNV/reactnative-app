74a79bcb914378c05a22d08f501b503312d025fb
Sriram <sriramnvilvanathan@gmail.com>
2025-11-29 18:56:05 +0530

    updated nextwind workings

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,21 @@
-import { Stack } from "expo-router";
+import { useFonts } from 'expo-font';
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from 'react';
 import "./global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded, error] = useFonts({
+    "Quicksand-Bold": require('../assets/fonts/Quicksand-Bold.ttf'),
+    "Quicksand-Medium": require('../assets/fonts/Quicksand-Medium.ttf'),
+    "Quicksand-Regular": require('../assets/fonts/Quicksand-Regular.ttf'),
+    "Quicksand-SemiBold": require('../assets/fonts/Quicksand-SemiBold.ttf'),
+    "Quicksand-Light": require('../assets/fonts/Quicksand-Light.ttf'),
+  });
+
+  useEffect(() => {
+    if(error) throw error;
+    if(fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error]);
+
+  return <Stack screenOptions={{headerShown: false}}/>;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,26 @@
-import { Text, View } from "react-native";
+import { offers } from "@/constants";
+import { FlatList, Pressable, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import "./global.css";
  
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
-      </Text>
-    </View>
+    <SafeAreaView>
+      <FlatList 
+        data={offers} 
+        renderItem={({item, index}) => {
+          return(
+            <View>
+              <Pressable className="bg-amber-600 my-3 h-48 rounded-xl">
+                 <Text>{item.title}</Text>
+                 
+              </Pressable>
+            </View>
+          )
+        }}
+      />
+        
+      
+    </SafeAreaView>
   );
 }
