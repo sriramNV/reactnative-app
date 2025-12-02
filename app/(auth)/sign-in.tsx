03371a62c718091699f809f674b914f4ef9cb28f
Sriram <sriramnvilvanathan@gmail.com>
2025-12-02 20:44:49 +0530

    finished sign-in page ui

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,28 +1,55 @@
 import CustomButton from '@/components/CustomButton';
 import CustomInput from '@/components/CustomInput';
-import { Link } from 'expo-router';
-import { Text, View } from 'react-native';
+import { Link, router } from 'expo-router';
+import { useState } from 'react';
+import { Alert, Text, View } from 'react-native';
 
 const SignIn = () => {
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [form, setForm] = useState({email: '', password: ''});
+
+  const submit = async () => {
+    if(!form.email || !form.password) Alert.alert('Error', 'Please enter valid Email and Password');
+
+    setIsSubmitting(true);
+
+    try{
+      // Appwrite sign in
+
+      Alert.alert('Success','Logged in successfully');
+      router.replace('/');
+    }
+    catch(e: any){
+      Alert.alert('Error', e.message);
+    }
+    finally{
+      setIsSubmitting(false);
+    }
+  }
+
   return (
     <View className='gap-10 bg-white rounded-lg p-5 mt-5'>
       
       <CustomInput 
             placeholder='Enter your email'
-            value=''
-            onChangeText={()=>{}}
+            value={form.email}
+            onChangeText={(text)=>setForm((prev)=>({...prev, email: text}))}
             label='Email'
             keyboardType='email-address'
       />
       <CustomInput 
             placeholder='Enter your password'
-            value=''
-            onChangeText={()=>{}}
+            value={form.password}
+            onChangeText={(pass)=>setForm((prev)=>({...prev, password: pass}))}
             label='Password'
             secureTextEntry={true}
       />
-      <CustomButton title='Sign In'/>
-      <View>
+      <CustomButton 
+        title='Sign In'
+        onPress={submit}
+      />
+      <View className='flex justify-center mt-5 flex-row gap-2'>
         <Text className='base-regular text-grey-100'>
             Don't have an account?
         </Text>
