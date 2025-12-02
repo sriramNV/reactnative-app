48394980c4b31aa7edf2adc251f9075b74e906fe
Sriram <sriramnvilvanathan@gmail.com>
2025-12-02 20:04:30 +0530

    partially completed sign-up page/screen

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,13 +1,39 @@
-import { router } from 'expo-router';
-import { Button, Text, View } from 'react-native';
+import CustomButton from '@/components/CustomButton';
+import CustomInput from '@/components/CustomInput';
+import { Link } from 'expo-router';
+import { Text, View } from 'react-native';
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>sign-in</Text>
-      <Button title='Sign Up' onPress={() => router.push("/(auth)/sign-up")} />
+    <View className='gap-10 bg-white rounded-lg p-5 mt-5'>
+      
+      <CustomInput 
+            placeholder='Enter your email'
+            value=''
+            onChangeText={()=>{}}
+            label='Email'
+            keyboardType='email-address'
+      />
+      <CustomInput 
+            placeholder='Enter your password'
+            value=''
+            onChangeText={()=>{}}
+            label='Password'
+            secureTextEntry={true}
+      />
+      <CustomButton title='Sign In'/>
+      <View>
+        <Text className='base-regular text-grey-100'>
+            Don't have an account?
+        </Text>
+        <Link href="/(auth)/sign-up" className='base-bold text-primary'>
+          Sign Up
+        </Link>
+      </View>
     </View>
   )
 }
 
 export default SignIn;
+
+// ?1:08:34
--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -2,4 +2,6 @@ export const appwriteConfig = {
     endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
     projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
     platform: "com.sstudio.foodorder",
+    databaseId: '692ee787003b7b48b511',
+    userTableId: 'user',
 }
